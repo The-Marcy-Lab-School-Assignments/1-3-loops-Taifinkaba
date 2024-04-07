966bf96dac60902ab7996eecd0f5ed9fe8b78a75
Taifinkaba <taifinkabad@gmail.com>
2024-04-07 19:18:03 -0400

    deleted comments on modify.js so test passes

--- src/modify.js
+++ src/modify.js
@@ -2,11 +2,6 @@ const whileToFor = () => {
   for(let i = 0; i < 5; i++){
     console.log(i);
   }
-  // let i = 0;
-  // while (i < 5) {
-  //   console.log(i);
-  //   i++;
-  // }
 };
 // whileToFor();
 
@@ -19,10 +14,6 @@ const continueGuardClause = () => {
     console.log(i); 
    }
 };
-  //   if (!(i === 2) && !(i === 3)) {
-  //     console.log("Sure glad this isn't 2 or 3");
-  //     console.log(i);
-  //   }
 // continueGuardClause();
 
 module.exports = {
