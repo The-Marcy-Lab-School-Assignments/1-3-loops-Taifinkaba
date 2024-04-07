360cbd3477102446a846baa6254930eaeef336b3
Taifinkaba <taifinkabad@gmail.com>
2024-04-07 18:11:19 -0400

    question 7 done

--- src/modify.js
+++ src/modify.js
@@ -8,16 +8,22 @@ const whileToFor = () => {
   //   i++;
   // }
 };
-whileToFor();
+// whileToFor();
 
 const continueGuardClause = () => {
   for (let i = 1; i < 5; i++) {
-    if (!(i === 2) && !(i === 3)) {
-      console.log("Sure glad this isn't 2 or 3");
-      console.log(i);
+    if(i === 2 || i === 3){
+      continue;
     }
-  }
+    console.log("Sure glad this isn't 2 or 3");
+    console.log(i); 
+   }
 };
+  //   if (!(i === 2) && !(i === 3)) {
+  //     console.log("Sure glad this isn't 2 or 3");
+  //     console.log(i);
+  //   }
+// continueGuardClause();
 
 module.exports = {
   whileToFor,
