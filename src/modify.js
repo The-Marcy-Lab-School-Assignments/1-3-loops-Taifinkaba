a8d4d712204df64ce445eacad27049454aba777c
Taifinkaba <taifinkabad@gmail.com>
2024-04-08 11:55:29 -0400

    changed Q7 for  readability

--- src/modify.js
+++ src/modify.js
@@ -7,14 +7,12 @@ const whileToFor = () => {
 
 const continueGuardClause = () => {
   for (let i = 1; i < 5; i++) {
-    if(i === 2 || i === 3){
-      continue;
-    }
+    if(i === 2 || i === 3)  continue;
     console.log("Sure glad this isn't 2 or 3");
     console.log(i); 
    }
 };
-// continueGuardClause();
+continueGuardClause();
 
 module.exports = {
   whileToFor,
